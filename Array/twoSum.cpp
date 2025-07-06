47985e44308a3029dd3eccca0abc6ffb104633b0
akhilanil2005 <akhilgokulam2005@gmail.com>
2025-07-06 12:31:24 +0530

    commit algorithm

--- Array/twoSum.cpp
+++ Array/twoSum.cpp
@@ -65,4 +65,31 @@ int main() {
 
 
 
-/*hello*/
+/*hello
+Algorithm
+Let's define a function that helps us find the k 
+th
+  smallest element from two inclusive ranges [a_start, a_end] and [b_start, b_end] from arrays A and B.
+
+If the range (for example, a range of A) is empty, in other words a_start > a_end, it means all elements in A are passed, we just return the (k - a_start)-th element from the other array B. Vice versa if b_start > b_end.
+
+Otherwise, get the middle indexes of the two ranges: a_index = (a_start + a_end) / 2, b_index = (b_start + b_end) / 2.
+
+Get the middle values of the two ranges: a_value = A[a_index], b_value = B[b_index].
+
+Cut one array in half, according to:
+
+If a_index + b_index < k, cut one smaller half.
+If a_value < b_value, cut the smaller half of A.
+Otherwise, cut the smaller half of B.
+Otherwise, cut one larger half.
+If b_value < a_value, cut the larger half of B.
+Otherwise, cut the larger half of A.
+Repeat step 1 using the new starting and ending indexes of A and B.
+
+Then we move on to find the median elements, and get the length of both arrays na = len(A) and nb = len(B).
+
+If the total number of elements in A and B is odd, we just use the above function to find the middle element, that is k = (na + nb) / 2.
+Otherwise, we use the function to find two middle elements: k = (na + nb) / 2 - 1 and k = (na + nb) / 2, and return their average.
+
+*/
